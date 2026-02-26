9bc8cd408d2982dcbcb7052dfbf03c0a40868ece
whelanmike <whelanmike@users.noreply.github.com>
2026-02-26 10:02:41 +0000

    Update duckdb_create_view_from_csv_file.sql

--- duckdb_create_view_from_csv_file.sql
+++ duckdb_create_view_from_csv_file.sql
@@ -1,7 +1,7 @@
 -- Requires duckDB version v1.4.1 or later.
 -- This version gets the following information from the csv_sniffer (Also works for .gz files)
---       (i) column names
---      (ii) data types
+--     (i)   column names
+--     (ii)  data types
 --     (iii) delimiter
 -- BEWARE of the consequences of 'store_rejects' setting. https://duckdb.org/docs/stable/data/csv/reading_faulty_csv_files#reject-errors
 /*
@@ -10,12 +10,13 @@
   -- set file_search_path = '/path_to_csv_file_directory'
   select 
         definition
-  from  generate_table_from_csv_normalize_cols('some_data.csv');
+  from  generate_table_from_csv_normalize_cols('some_data.csv');   -- or .gz file.
   2)
   - Query 1 generates SQL code to create view from csv file.
-  - Copy & Pase output to query editor.
+  - Copy & Paste output to query editor.
   - Review all column names and data types.
   - Search/Replace '--:' to use the normalised column names, e.g. ColumnName --> column_name.
+  - ** 'store_rejects' option can mask errors. It's useful for debugging. Execute `select * from reject_errors` after running view.
 */
 
 create or replace macro generate_table_from_csv_normalize_cols(csv_file_name) as table
@@ -23,7 +24,8 @@ create or replace macro generate_table_from_csv_normalize_cols(csv_file_name) as
         (
         select 
                delimiter
-              ,unnest(col_name_and_type) 
+              ,json_extract_string(col_name_and_type, '$.name')  as column_name
+              ,json_extract_string(col_name_and_type, '$.type')  as data_type
         from  (
               select 
                      delimiter
@@ -38,58 +40,39 @@ create or replace macro generate_table_from_csv_normalize_cols(csv_file_name) as
         ,c_col_metadata as
         (
         select 
-               format('{:02d}', row_number() over())                               as col_id
-              ,name                                                                as column_name
-              ,regexp_replace(name, '([a-z0-9])([A-Z])', '\1_\2', 'g').lower()     as col_name_normalized  -- e.g. UserName --> user_name
-              ,len(name)                                                           as col_len
-              ,max(col_len) over()                                                 as mx_len
-              ,repeat(' ', (mx_len - col_len) + 3)                                 as spacing
-              ,type                                                                as column_type
+               col_id             : format('{:02d}', row_number() over())                               
+              ,column_name                                                                        
+              ,col_name_normalized: regexp_replace(column_name, '([a-z0-9])([A-Z])', '\1_\2', 'g').lower()      -- e.g. UserName --> user_name
+              ,col_len            : len(column_name)                                                           
+              ,mx_len             : max(col_len) over()                                                 
+              ,spacing            : repeat(' ', (mx_len - col_len) + 3)                                 
+              ,data_type                                                                
         from  c_table_metadata
         )
-        select 'create or replace view v_' || lower(replace(csv_file_name, '.csv', '')) ||  ' as  -- **** RENAME AS REQUIRED ****' as definition
-        union all
-        select '    select '
-        union all
-        select 
-              case when col_id =1 then '           ' else '          ,' end   || (column_name) || spacing ||  '--: as '  || lower(col_name_normalized) 
-        from  c_col_metadata
-        union all 
-        select '    from  read_csv'
-        union all 
-        select '              (''' ||  csv_file_name || '''' 
-        union all            
-        select '              ,sep='  || '''' ||  concat(delimiter, '''                               -- Using sniffed value of delimiter.') from c_delim 
-        union all            
-        select '              --,quote=''"' || '''' || '                           -- Include if csv data is quoted.'
-        union all            
-        select '              --,DATEFORMAT = ''' || '%m/%d/%Y' || ''''  || '             -- Convert Dates to ISO-8601 format.' 
-        union all            
-        select '              ,header = True' || '                         -- comment out if no field names in file. Required names & types can be added in columns section of generated SQL.'
-        union all            
-        select '              ,store_rejects = True' || '                  -- select * from reject_errors;'
-        union all            
-        select '              --,ignore_errors=True' || '                  -- Mutually Exclusive with store_rejects option.'
-        union all            
-        select '              --,nullstr=''NA'''  || '                        -- Use to convert string to null e.g. N\A, NA, etc.'
-        union all            
-        select '              ,columns = ' || '                            -- Data types are not always inferred correctly. Validate column list below. '
-        union all            
-        select '                  { '       
-        union all            
-        select 
-              case when col_id =1 then '                   ' else '                  ,' end   ||  ''''  || (column_name) || '''' 
-              || spacing || ':''' || column_type || ''''  || repeat(' ', 12-len(column_type)) || '-- (' || col_id || ')'
-              as output
-        from  c_col_metadata
-        union all 
-        select '                  } '       
-        union all 
-        select '              )'      
-        union all 
-        select ';' 
-        union all 
-        select '' 
-        union all 
-        select 'select * from reject_errors;  -- Check for loading errors:- run this after querying the view created above.'            
-
+                  select 'create or replace view v_' || lower(replace(csv_file_name, '.csv', '')) ||  ' as  -- **** RENAME AS REQUIRED ****' as definition
+        union all select '    select '
+        union all select 
+                        case when col_id =1 then '           ' else '          ,' end   || (column_name) || spacing ||  '--: as '  || lower(col_name_normalized) 
+                  from  c_col_metadata
+        union all select '    from  read_csv'
+        union all select '              (''' ||  csv_file_name || '''' 
+        union all select '              ,sep='  || '''' ||  concat(delimiter, '''                               -- Using sniffed value of delimiter.') from c_delim 
+        union all select '              --,quote=''"' || '''' || '                           -- Include if csv data is quoted.'
+        union all select '              --,DATEFORMAT = ''' || '%m/%d/%Y' || ''''  || '             -- Convert Dates to ISO-8601 format.' 
+        union all select '              ,header = True' || '                         -- comment out if no field names in file. Required names & types can be added in columns section of generated SQL.'
+        union all select '              ,store_rejects = True' || '                  -- union all select * from reject_errors;'
+        union all select '              --,ignore_errors=True' || '                  -- Mutually Exclusive with store_rejects option.'
+        union all select '              --,nullstr=''NA'''  || '                        -- Use to convert string to null e.g. N\A, NA, etc.'
+        union all select '              ,columns = ' || '                            -- Data types are not always inferred correctly. Validate column list below. '
+        union all select '                  { '       
+        union all select 
+                        case when col_id =1 then '                   ' else '                  ,' end   ||  ''''  || (column_name) || '''' 
+                        || spacing || ':''' || data_type || ''''  || repeat(' ', 12-len(data_type)) || '-- (' || col_id || ')'
+                        as output
+                  from  c_col_metadata
+        union all select '                  } '       
+        union all select '              )'      
+        union all select ';' 
+        union all select '' 
+        union all select 'select * from reject_errors;  -- Check for loading errors:- run this after querying the view created above.'            
+;        
